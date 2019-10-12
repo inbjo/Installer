92c1190afad23c715e1011820bddf1eb4ddd9e30
Flex <2345@mail.com>
2019-10-12 14:49:00 +0800

    Update DatabaseController.php

--- src/Controllers/DatabaseController.php
+++ src/Controllers/DatabaseController.php
@@ -2,10 +2,7 @@
 
 namespace Flex\Installer\Controllers;
 
-use App\Models\User;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Carbon;
 use Flex\Installer\Helpers\DatabaseManager;
 
 class DatabaseController extends Controller
@@ -30,22 +27,7 @@ class DatabaseController extends Controller
      */
     public function database()
     {
-        $admin = Cache::pull('admin');
-
         $response = $this->databaseManager->migrateAndSeed();
-
-        //设置管理员账号密码
-        User::updateOrCreate(
-            ['id' => 1],
-            [
-                'name' => $admin['name'],
-                'email' => $admin['email'],
-                'avatar' => generateAvatar($admin['email']),
-                'password' => bcrypt($admin['password']),
-                'bio' => '这家伙很懒什么也没写~',
-                'email_verified_at' => Carbon::now()->toDateTimeString(),
-            ]
-        );
 
         return redirect()->route('LaravelInstaller::final')
             ->with(['message' => $response]);
